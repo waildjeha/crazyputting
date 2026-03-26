f59c8d9f357823cef119a31f5b0e8023c4ceea3c
Egilmez, Bekir (Stud. DACS) <b.egilmez@student.maastrichtuniversity.nl>
2026-03-26 00:34:58 +0000

    The main to run my code

---
+++ Main.java
@@ -0,0 +1,24 @@
+import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application{
+    
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("MainScene.fxml"));
+        Parent root = loader.load();
+        Controller controller = loader.getController(); //Now The controller can modify the scene
+        controller.setMainWindow(primaryStage);
+        primaryStage.setTitle("Beko");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
