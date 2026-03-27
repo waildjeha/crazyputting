60561626e0ea379f1db50d9c8f53124c7f2ab53d
Egilmez, Bekir (Stud. DACS) <b.egilmez@student.maastrichtuniversity.nl>
2026-03-27 05:22:07 +0000

    Edit Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,4 @@
 import javafx.application.Application;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -13,7 +12,7 @@ public class Main extends Application{
         Parent root = loader.load();
         Controller controller = loader.getController(); //Now The controller can modify the scene
         controller.setMainWindow(primaryStage);
-        primaryStage.setTitle("Beko");
+        primaryStage.setTitle("Project 1-2 : Phase 1");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
